ccc7322b16fb6026abff8bf9775460af0190ad91
1693905917 <1693905917@qq.com>
2023-10-06 16:09:47 +0800

    二叉树-根据关键字删除

--- src/main/java/com/hzp/algorithm/binarysearchtree/BSTTree1.java
+++ src/main/java/com/hzp/algorithm/binarysearchtree/BSTTree1.java
@@ -56,7 +56,12 @@ public class BSTTree1 {
     }
 
     //查询方法：非递归实现
-
+    /**
+     * <h3>查找关键字对应的值</h3>
+     *
+     * @param key 关键字
+     * @return 关键字对应的值
+     */
     public Object get(int key) {
         BSTNode node = root;
         while (node != null) {
@@ -101,6 +106,11 @@ public class BSTTree1 {
 //        return p.value;
     }
 
+    /**
+     * <h3>查找最小关键字对应值</h3>
+     *
+     * @return 关键字对应的值
+     */
     private Object min(BSTNode node){
         if (node == null) {
             return null;
@@ -143,6 +153,11 @@ public class BSTTree1 {
     }
 
 
+    /**
+     * <h3>查找最大关键字对应值</h3>
+     *
+     * @return 关键字对应的值
+     */
     //通用的Max方法
     private Object max(BSTNode node){
         if (node == null) {
@@ -158,6 +173,12 @@ public class BSTTree1 {
 
     //1.key在该二叉树中有 那就更新key所对应的value值
     //2.key在该二叉树中没有 那就新增key与所对应的value值
+    /**
+     * <h3>存储关键字和对应值</h3>
+     *
+     * @param key   关键字
+     * @param value 值
+     */
     public void put(int key, Object value) {
         BSTNode node = root;
         BSTNode parent = null;
@@ -184,7 +205,13 @@ public class BSTTree1 {
         }
     }
 
-    public Object predecessor(int key) {
+    /**
+     * 查找关键字的前任值
+     *
+     * @param key 关键字
+     * @return 前任值 比如：4的前任是3  7的前任是6
+     */
+   public Object predecessor(int key) {
         BSTNode ancestorFromLeft = null;
         BSTNode p = root;
         //查找用户给的Key在二叉树中是否有
@@ -218,7 +245,12 @@ public class BSTTree1 {
         return ancestorFromLeft != null ? ancestorFromLeft.value : null;
     }
 
-
+    /**
+     * <h3>查找关键字的后任值</h3>
+     *
+     * @param key 关键字
+     * @return 后任值
+     */
     public Object successor(int key) {
         BSTNode ancestorFromRight = null;
         BSTNode p = root;
@@ -242,6 +274,75 @@ public class BSTTree1 {
         }
         // 情况2 - 有祖先自右而来
         return ancestorFromRight != null ? ancestorFromRight.value : null;
+    }
+
+
+    /**
+     * <h3>根据关键字删除</h3>
+     *
+     * @param key 关键字
+     * @return 被删除关键字对应值
+     */
+    public Object delete(int key) {
+        BSTNode p = root;
+        BSTNode parent = null;//记录待删除节点的父亲
+        while (p != null) {
+            if (key < p.key) {
+                parent = p;
+                p = p.left;
+            } else if (p.key < key) {
+                parent = p;
+                p = p.right;
+            } else {
+                break;
+            }
+        }
+        if (p == null) {
+            return null;
+        }
+        // 删除操作
+        if (p.left == null) {
+            shift(parent, p, p.right); // 情况1
+        } else if (p.right == null) {
+            shift(parent, p, p.left); // 情况2
+        } else {
+            // 情况4：被删除节点是左、右都有子节点，所以找后继节点：节点有右子树，此时后继节点即为右于树的最小值
+            // 4.1 被删除节点找后继
+            BSTNode s = p.right;//后继节点
+            BSTNode sParent = p; // 后继父亲
+            while (s.left != null) {  //当循环结束，后继节点即为s
+                sParent = s;
+                s = s.left;
+            }
+            // 4.2 删除和后继不相邻, 处理后继的后事
+            if (sParent != p) {
+                shift(sParent, s, s.right); // 不可能有左孩子：因为节点有右子树，此时后继节点即为右于树的最小值
+                s.right = p.right;//顶上去的后继节点的右孩子==被删除节点的右孩子
+            }
+            // 4.3 后继取代被删除节点
+            shift(parent, p, s);
+            s.left = p.left;//shift方法只改变了父类节点的左右孩子的指向，而没有改变你后继节点的做左右孩子的指向
+        }
+        return p.value;
+    }
+
+    /**
+     * 托孤方法
+     *
+     * @param parent  被删除节点的父亲
+     * @param deleted 被删除节点
+     * @param child   被顶上去的节点
+     */
+// 只考虑让 n1父亲的左或右孩子指向 n2, n1自己的左或右孩子并未在方法内改变
+    private void shift(BSTNode parent, BSTNode deleted, BSTNode child) {
+        //情况讨论：当被删除节点就是根节点，而根节点是没有父亲的
+        if (parent == null) {
+            root = child;//你们被删除节点的子节点就成根节点
+        } else if (deleted == parent.left) {
+            parent.left = child;
+        } else {
+            parent.right = child;
+        }
     }
 
 
